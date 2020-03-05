a0199d9487f8ef9812517c8baa4e60b2095003f3
MathieuKruk <mathieukruk@gmail.com>
2020-03-05 18:11:36 +0100

    managing structure

---
+++ ue-app/build/precache-manifest.e0c973b88e071f764fefb285ee9fc5fa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deab1494f704565897f44677cd743856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c64b7d3c0276f4527137",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fdec619df7bf8baf699c",
+    "url": "/static/css/main.54ae0b37.chunk.css"
+  },
+  {
+    "revision": "c64b7d3c0276f4527137",
+    "url": "/static/js/2.3bee7515.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.3bee7515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdec619df7bf8baf699c",
+    "url": "/static/js/main.05c7c126.chunk.js"
+  },
+  {
+    "revision": "85f5b80fb05510156435",
+    "url": "/static/js/runtime-main.42eca0d9.js"
+  },
+  {
+    "revision": "eab06f72014ab6bbc62b8ec437f879a8",
+    "url": "/static/media/Crowd1.eab06f72.png"
+  },
+  {
+    "revision": "a27d5b3a7eb4b70c9a8b8a7fe9e5dde5",
+    "url": "/static/media/Crowd2.a27d5b3a.png"
+  },
+  {
+    "revision": "944e82730f45591b210e5272670445a7",
+    "url": "/static/media/TitleExplorers.944e8273.svg"
+  },
+  {
+    "revision": "a33450091a551dca78cf6339e0e00567",
+    "url": "/static/media/TitleUrban.a3345009.svg"
+  },
+  {
+    "revision": "91ae0e24090ac875ac206153644779cd",
+    "url": "/static/media/Vinyle.91ae0e24.svg"
+  },
+  {
+    "revision": "747c5a35546e87a658041974ad5689bc",
+    "url": "/static/media/news1.747c5a35.jpg"
+  },
+  {
+    "revision": "78527ac25b4408f7fcce6fc7ef433ee6",
+    "url": "/static/media/news2.78527ac2.jpg"
+  },
+  {
+    "revision": "da01bd85fa47f96eb7ab5a2375dcd494",
+    "url": "/static/media/news3.da01bd85.jpg"
+  },
+  {
+    "revision": "6e6af5f8b225e2a61705fbd6b9c5f4ff",
+    "url": "/static/media/skateboard.6e6af5f8.svg"
+  },
+  {
+    "revision": "3aa9859153fe25246493132261c8e6e2",
+    "url": "/static/media/spray.3aa98591.svg"
+  },
+  {
+    "revision": "3179a74e3bd02d7e9191db057dc23d25",
+    "url": "/static/media/synthesizer.3179a74e.svg"
+  }
+]);
